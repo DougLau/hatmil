187b1c5ab00fafa94ba7c3f74f86b73daea0e32a
Doug Lau <doug.p.lau@gmail.com>
2025-08-04 12:38:56 -0500

    Reordered macros

--- src/html.rs
+++ src/html.rs
@@ -366,18 +366,6 @@ macro_rules! elements {
     }
 }
 
-elements![
-    a, abbr, address, article, aside, audio, b, bdi, bdo, blockquote, body, br,
-    button, canvas, caption, cite, code, colgroup, data, datalist, dd, del,
-    details, dfn, dialog, div, dl, dt, em, fieldset, figcaption, figure,
-    footer, form, h1, h2, h3, h4, h5, h6, head, header, hgroup, html, i,
-    iframe, ins, kbd, label, legend, li, main, map, mark, menu, meter, nav,
-    noscript, object, ol, optgroup, option, output, p, picture, pre, progress,
-    q, rp, rt, ruby, s, samp, script, search, section, select, slot, small,
-    span, strong, style, sub, summary, sup, table, tbody, td, template,
-    textarea, tfoot, th, thead, time, title, tr, u, ul, var, video
-];
-
 /// HTML Void element helper
 macro_rules! void_elements {
     ( $( $elem:ident ),* ) => {
@@ -403,6 +391,18 @@ macro_rules! void_elements {
         }
     }
 }
+
+elements![
+    a, abbr, address, article, aside, audio, b, bdi, bdo, blockquote, body, br,
+    button, canvas, caption, cite, code, colgroup, data, datalist, dd, del,
+    details, dfn, dialog, div, dl, dt, em, fieldset, figcaption, figure,
+    footer, form, h1, h2, h3, h4, h5, h6, head, header, hgroup, html, i,
+    iframe, ins, kbd, label, legend, li, main, map, mark, menu, meter, nav,
+    noscript, object, ol, optgroup, option, output, p, picture, pre, progress,
+    q, rp, rt, ruby, s, samp, script, search, section, select, slot, small,
+    span, strong, style, sub, summary, sup, table, tbody, td, template,
+    textarea, tfoot, th, thead, time, title, tr, u, ul, var, video
+];
 
 void_elements![
     area, base, col, embed, hr, img, input, link, meta, source, track, wbr
